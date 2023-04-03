19fd518c22cf45b2b7f5445437be4c0ae232e138
Guilherme909871 <guilherme909871@gmail.com>
2023-04-03 18:45:56 -0300

    updates
    turning 12 lines of code into one and making it compatible with more systems and standard browsers.

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import time
 import pyautogui
 from tkinter import*
+import webbrowser
 
 def youtube():
-    pyautogui.PAUSE = 0.3
-    pyautogui.hotkey("win", "r")
-    pyautogui.write("cmd")
-    pyautogui.press("enter")
-    pyautogui.write("start www.youtube.com")
-    pyautogui.press("enter")
-    time.sleep(1)
+    webbrowser.open("https://youtube.com")
+    time.sleep(2)
     pyautogui.press("f11")
     time.sleep(2)
     pyautogui.click(x=1751, y=25)
@@ -18,12 +14,7 @@ def youtube():
 
 
 def instagram():
-    pyautogui.PAUSE = 0.3
-    pyautogui.hotkey("win", "r")
-    pyautogui.write("cmd")
-    pyautogui.press("enter")
-    pyautogui.write("start www.instagram.com")
-    pyautogui.press("enter")
+    webbrowser.open("https://instagram.com")
     time.sleep(2)
     pyautogui.press("f11")
     time.sleep(2)
@@ -35,17 +26,21 @@ def instagram():
 
 Janela = Tk()
 Janela.title("automatizar insta e shorts")
-Janela.geometry("500x500")
-Texto = Label(Janela, text="Clique no Botão e rode o programa")
-Texto.grid(column=1, row=1)
+Janela.geometry("800x800")
+Texto = Label(Janela, text="Clique no Botão e rode o programa", bg="yellow", fg="red")
+Texto.grid(column=3,row=2)
+imagem2 = PhotoImage(file="n.png")
+imagem = Label(Janela, image=imagem2)
+imagem.grid(column=0,row=0)
+
 logo2 = PhotoImage(file="reels.png",width=180, height=100)
 botao = Button(Janela,image=logo2 ,command=lambda:instagram())
 logo = PhotoImage(file="shorts.png", width=180, height= 100)
 logo.subsample(3,3)
 
-botao.grid(column=1, row=2)
+botao.grid(column=3, row=4)
 botao2 = Button(Janela,image=logo, command=lambda:youtube())
-botao2.grid(column=2, row=2)
+botao2.grid(column=4, row=4)
 def sair():
     Janela.destroy()
 
