aade49572c3f22ad01ecf211e5d4f8742b76fb39
Guilherme909871 <guilherme909871@gmail.com>
2023-04-05 20:11:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,18 @@ import webbrowser
 
 def youtube():
     webbrowser.open("https://youtube.com")
-    time.sleep(2)
-    pyautogui.press("f11")
-    time.sleep(2)
-    pyautogui.click(x=1751, y=25)
+    time.sleep(3)
+    pyautogui.click(x=1748, y=93)
     time.sleep(1)
-    pyautogui.click(x=1660, y=73)
+    pyautogui.click(x=1658, y=136)
 
 
 def instagram():
     webbrowser.open("https://instagram.com")
     time.sleep(2)
-    pyautogui.press("f11")
-    time.sleep(2)
-    pyautogui.click(x=62, y=464)
+    pyautogui.click(x=39, y=525)
     time.sleep(1)
-    pyautogui.click(x=942, y=626)
+    pyautogui.click(x=943, y=636)
 
 
 
@@ -29,10 +25,6 @@ Janela.title("automatizar insta e shorts")
 Janela.geometry("800x800")
 Texto = Label(Janela, text="Clique no Botão e rode o programa", bg="yellow", fg="red")
 Texto.grid(column=3,row=2)
-#2 = PhotoImage(file="n.png")
-#imagem = Label(Janela, image=imagem2)
-#imagem.grid(column=0,row=0)
-
 logo2 = PhotoImage(file="reels.png",width=180, height=100)
 botao = Button(Janela,image=logo2 ,command=lambda:instagram())
 logo = PhotoImage(file="shorts.png", width=180, height= 100)
